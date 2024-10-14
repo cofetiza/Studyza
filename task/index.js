934f4249b80b84adf219859b3d59ac2263b96217
amnucode <abdul1024malik@gmail.com>
2024-10-14 20:30:47 +0700

    ongoing tasks features

--- task/index.js
+++ task/index.js
@@ -1,16 +1,95 @@
 let tasks = {
-  todo: []
+  todo: [],
+  ongoing: [],
+  done: []
 };
 if (localStorage.tasks !== undefined) {
   tasks = JSON.parse(localStorage.tasks);
 }
+let selectId = ["none"];
+
+const select = (x,y) => {
+  selectId = [x,y];
+  refresh();
+}
 
 const refresh = () => {
+  localStorage.tasks = JSON.stringify(tasks);
   let render = '';
-  tasks.todo.forEach(task => {
-    render += `<div>${task.title}</div>`;
+  tasks.todo.forEach((task,id) => {
+    if (selectId[0] === 'todo' && selectId[1] === id) {
+      render += `
+        <div class="vertical-space"></div>
+        <div class="task-container">
+          <div class="task-item">${task.description}</div>
+          <button onClick="move('todo',${id},'none')">Cancel task</button>
+          <button onClick="move('todo',${id},'ongoing')">Do now</button>
+        </div>
+      `;
+    }
+    else {
+      render += `
+        <div class="vertical-space"></div>
+        <div class="task-container" onClick="select('todo',${id})">
+          <div class="task-item">${task.title}</div>
+        </div>
+      `;
+    }
   });
   document.getElementById('todo').innerHTML = render;
+  render = '';
+  tasks.ongoing.forEach((task,id) => {
+    if (selectId[0] === 'ongoing' && selectId[1] === id) {
+      render += `
+        <div class="vertical-space"></div>
+        <div class="task-container">
+          <div class="task-item">${task.description}</div>
+          <button onClick="move('ongoing',${id},'todo')">Cancel task</button>
+          <button onClick="move('ongoing',${id},'done')">Finish</button>
+        </div>
+      `;
+    }
+    else {
+      render += `
+        <div class="vertical-space"></div>
+        <div class="task-container" onClick="select('ongoing',${id})">
+          <div class="task-item">${task.title}</div>
+        </div>
+      `;
+    }
+  });
+  document.getElementById('ongoing').innerHTML = render;
+  render = '';
+  tasks.done.forEach((task,id) => {
+    if (selectId[0] === 'done' && selectId[1] === id) {
+      render += `
+        <div class="vertical-space"></div>
+        <div class="task-container">
+          <div class="task-item">${task.description}</div>
+          <button onClick="move('done',${id},'ongoing')">Continue</button>
+          <button onClick="move('done',${id},'none')">Remove</button>
+        </div>
+      `;
+    }
+    else {
+      render += `
+        <div class="vertical-space"></div>
+        <div class="task-container" onClick="select('done',${id})">
+          <div class="task-item">${task.title}</div>
+        </div>
+      `;
+    }
+  });
+  document.getElementById('done').innerHTML = render;
+}
+
+const move = (a,id,b) => {
+  if (b !== 'none') {
+    tasks[b].push(tasks[a][id]);
+  }
+  tasks[a].splice(id,1);
+  selectId = ['none'];
+  refresh();
 }
 
 const create = () => {
@@ -25,7 +104,6 @@ const create = () => {
     alert("Description can not empty.");
     return;
   }
-  console.log(deadline);
   if (deadline === "") {
     alert("Deadline can not empty.");
     return;
@@ -35,7 +113,6 @@ const create = () => {
     description: description,
     deadline: deadline
   });
-  localStorage.tasks = JSON.stringify(tasks);
   refresh();
   alert("Task created.");
 }
